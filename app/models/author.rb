8167682519a0448b37262bbd57e06eee3bc08683
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-19 22:37:42 +0200

    validate comments

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
   has_many :posts
+  has_many :comments
+  
 
   def full_name
     "#{self.first_name} #{self.last_name} "
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, required: true
+  belongs_to :author, required: true
   enum status: [ :published, :unpublished ]
+  validates :body, length: { minimum: 5 }
 end
