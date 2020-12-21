d8e5e9c2e65d700a1df01236491df3c169260c51
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-21 12:26:37 +0200

    update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,16 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post), notice: 'Comment was successfully updated.' 
+    else
+      redirect_to post_path(@post)
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body, :author_id)
